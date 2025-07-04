9be5729046876eb87648c9d8bd78fd420401136b
gabriel <gabriel.salomon.2007@gmail.com>
2025-07-04 11:24:27 -0400

    [README-07] The RepositoriesSummary.test.js was updated.

--- 8codesplain-act/src/components/repositories/RepositoriesSummary.test.js
+++ 8codesplain-act/src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from '@testing-library/react';
 import RepositoriesSummary from './RepositoriesSummary';
 
-test('displays the primary language of the repository', () => {
+test('displays information about the repository', () => {
   const repository = {
     language: 'Javascript',
     stargazers_count: 5,
@@ -10,7 +10,10 @@ test('displays the primary language of the repository', () => {
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText('Javascript');
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
 
-  expect(language).toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  }
 });
