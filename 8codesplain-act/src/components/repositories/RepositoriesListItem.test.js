9c4b0990aab94f53d50412c99117b2fddf0ddde1
gabriel <gabriel.salomon.2007@gmail.com>
2025-07-04 11:24:40 -0400

    [README-07] The RepositoriesListItem.test.js was created.

---
+++ 8codesplain-act/src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import RepositoriesListItem from './RepositoriesListItem';
+
+function renderComponent() {
+  const repository = {
+    full_name: 'facebook/react',
+    language: 'Javascript',
+    description: 'A js library',
+    owner: 'facebook',
+    name: 'react',
+    html_url: 'https://github.com/facebook/react',
+  };
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+
+test('shows a link to the github homepage for this repository', () => {
+  renderComponent();
+});
