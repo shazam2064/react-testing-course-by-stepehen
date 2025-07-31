f3a39b40cc0e79acb7a8d1d6cbf37a40ca463970
gabriel <gabriel.salomon.2007@gmail.com>
2025-07-31 16:03:49 -0400

    [4react-products-2back] The user test was updated to fully test out the getUser By Id method

--- course-projects/4react-products-2back/tests/auth.test.js
+++ course-projects/4react-products-2back/tests/auth.test.js
@@ -2,157 +2,157 @@ const request = require('supertest');
 const app = require('../app');
 const User = require('../models/user.model');
 
-describe('Auth Login', () => {
-    it('should return 200 and a valid token with user details', async () => {
-        const requestBody = {
-            email: 'admin1@test.com',
-            password: '123456',
-        };
-
-        const response = await request(app)
-            .post('/auth/login')
-            .send(requestBody)
-            .set('Content-Type', 'application/json');
-
-        expect(response.status).toBe(200);
-
-        expect(response.headers['content-type']).toMatch(/application\/json/);
-
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                token: expect.any(String),
-                userId: expect.any(String),
-                email: 'admin1@test.com',
-                isAdmin: true,
-            })
-        );
-    });
-
-    it('should return 422 for invalid credentials', async () => {
-        const requestBody = {
-            email: 'admin1@test.com',
-            password: 'wrongpassword',
-        };
-
-        const response = await request(app)
-            .post('/auth/login')
-            .send(requestBody)
-            .set('Content-Type', 'application/json');
-
-        // Assert the correct status code
-        expect(response.status).toBe(422);
-
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                message: 'Wrong password',
-            })
-        );
-    });
-});
-
-describe('Auth Signup', () => {
-    it('should create a new user and return 201 with user details', async () => {
-        const requestBody = {
-            email: 'admin3@test.com',
-            name: 'User Test 3',
-            password: '123456',
-        };
-
-        const response = await request(app)
-            .put('/auth/signup')
-            .send(requestBody)
-            .set('Content-Type', 'application/json');
-
-        expect(response.status).toBe(201);
-
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                message: 'User created!',
-                userId: expect.any(String),
-            })
-        );
-    });
-
-
-    it('should return 422 if the email address already exists', async () => {
-        const requestBody = {
-            email: 'admin1@test.com',
-            name: 'User Test 1',
-            password: '123456',
-        };
-
-        const response = await request(app)
-            .put('/auth/signup')
-            .send(requestBody)
-            .set('Content-Type', 'application/json');
-
-        expect(response.status).toBe(422);
-
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                message: 'Validation failed, entered data is incorrect',
-                details: expect.arrayContaining([
-                    expect.objectContaining({
-                        type: 'field',
-                        value: 'admin1@test.com',
-                        msg: 'Email address already exists!',
-                        path: 'email',
-                        location: 'body',
-                    }),
-                ]),
-            })
-        );
-    });
-});
-
-describe('Auth GET Status', () => {
-    let validToken;
-
-    beforeAll(async () => {
-        const loginResponse = await request(app)
-            .post('/auth/login')
-            .send({
+describe('Auth Controller', () => {
+    describe('Auth Login', () => {
+        it('should return 200 and a valid token with user details', async () => {
+            const requestBody = {
                 email: 'admin1@test.com',
                 password: '123456',
-            })
-            .set('Content-Type', 'application/json');
+            };
 
-        expect(loginResponse.status).toBe(200);
-        validToken = loginResponse.body.token;
-    });
+            const response = await request(app)
+                .post('/auth/login')
+                .send(requestBody)
+                .set('Content-Type', 'application/json');
 
-    it('should return 200 and the user status if the user exists', async () => {
-        const mockUserId = 'validUserId';
+            expect(response.status).toBe(200);
 
-        jest.spyOn(User, 'findById').mockResolvedValueOnce({
-            _id: mockUserId,
-            status: 'Active',
+            expect(response.headers['content-type']).toMatch(/application\/json/);
+
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    token: expect.any(String),
+                    userId: expect.any(String),
+                    email: 'admin1@test.com',
+                    isAdmin: true,
+                })
+            );
         });
 
-        const response = await request(app)
-            .get('/auth/status')
-            .set('Authorization', `Bearer ${validToken}`);
+        it('should return 422 for invalid credentials', async () => {
+            const requestBody = {
+                email: 'admin1@test.com',
+                password: 'wrongpassword',
+            };
 
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                status: 'Active',
-            })
-        );
+            const response = await request(app)
+                .post('/auth/login')
+                .send(requestBody)
+                .set('Content-Type', 'application/json');
+
+            expect(response.status).toBe(422);
+
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    message: 'Wrong password',
+                })
+            );
+        });
     });
 
-    it('should return 404 if the user is not found', async () => {
-        jest.spyOn(User, 'findById').mockResolvedValueOnce(null);
+    describe('Auth Signup', () => {
+        it('should create a new user and return 201 with user details', async () => {
+            const requestBody = {
+                email: 'admin3@test.com',
+                name: 'User Test 3',
+                password: '123456',
+            };
 
-        const response = await request(app)
-            .get('/auth/status')
-            .set('Authorization', `Bearer ${validToken}`);
+            const response = await request(app)
+                .put('/auth/signup')
+                .send(requestBody)
+                .set('Content-Type', 'application/json');
 
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                message: 'User not found',
-            })
-        );
+            expect(response.status).toBe(201);
+
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    message: 'User created!',
+                    userId: expect.any(String),
+                })
+            );
+        });
+
+        it('should return 422 if the email address already exists', async () => {
+            const requestBody = {
+                email: 'admin1@test.com',
+                name: 'User Test 1',
+                password: '123456',
+            };
+
+            const response = await request(app)
+                .put('/auth/signup')
+                .send(requestBody)
+                .set('Content-Type', 'application/json');
+
+            expect(response.status).toBe(422);
+
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    message: 'Validation failed, entered data is incorrect',
+                    details: expect.arrayContaining([
+                        expect.objectContaining({
+                            type: 'field',
+                            value: 'admin1@test.com',
+                            msg: 'Email address already exists!',
+                            path: 'email',
+                            location: 'body',
+                        }),
+                    ]),
+                })
+            );
+        });
+    });
+
+    describe('Auth GET Status', () => {
+        let validToken;
+
+        beforeAll(async () => {
+            const loginResponse = await request(app)
+                .post('/auth/login')
+                .send({
+                    email: 'admin1@test.com',
+                    password: '123456',
+                })
+                .set('Content-Type', 'application/json');
+
+            expect(loginResponse.status).toBe(200);
+            validToken = loginResponse.body.token;
+        });
+
+        it('should return 200 and the user status if the user exists', async () => {
+            const mockUserId = 'validUserId';
+
+            jest.spyOn(User, 'findById').mockResolvedValueOnce({
+                _id: mockUserId,
+                status: 'Active',
+            });
+
+            const response = await request(app)
+                .get('/auth/status')
+                .set('Authorization', `Bearer ${validToken}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    status: 'Active',
+                })
+            );
+        });
+
+        it('should return 404 if the user is not found', async () => {
+            jest.spyOn(User, 'findById').mockResolvedValueOnce(null);
+
+            const response = await request(app)
+                .get('/auth/status')
+                .set('Authorization', `Bearer ${validToken}`);
+
+            expect(response.status).toBe(404);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    message: 'User not found',
+                })
+            );
+        });
     });
 });
