6016ea9053f6b63bcdcb8e4bf44fb7b076b6c950
gabriel <gabriel.salomon.2007@gmail.com>
2025-08-26 12:31:02 -0400

    [4react-products-1front] the setupTests.js was updated.

--- course-projects/4react-products-1front/src/setupTests.js
+++ course-projects/4react-products-1front/src/setupTests.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+if (!global.TextEncoder) global.TextEncoder = TextEncoder;
+if (!global.TextDecoder) global.TextDecoder = TextDecoder;
+
+// 👇 Fake stream classes so msw doesn’t crash
+if (!global.TransformStream) global.TransformStream = class {};
+if (!global.ReadableStream) global.ReadableStream = class {};
+if (!global.WritableStream) global.WritableStream = class {};
