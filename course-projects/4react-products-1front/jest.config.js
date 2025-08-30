0e6057294cb39bafa323cac022488fd5315efc61
gabriel <gabriel.salomon.2007@gmail.com>
2025-08-30 11:32:00 -0400

    [4react-products-1front] the jest.config.js was added and is working.

---
+++ course-projects/4react-products-1front/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+        '/node_modules/(?!(axios)/)',
+    ],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['<rootDir>/config/jest.setup.js'], // Correct usage of <rootDir>
+    testMatch: ['**/?(*.)+(test).[jt]s?(x)'], // Matches files like Login.test.js or Login.test.jsx
+};
