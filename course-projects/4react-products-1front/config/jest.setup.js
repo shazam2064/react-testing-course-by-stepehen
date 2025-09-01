5c3fc2a9106c2375c56ec3b83081d64508dda85c
gabriel <gabriel.salomon.2007@gmail.com>
2025-09-01 11:01:16 -0400

    [4react-products-1front] the jest setup was updated to handle more stuff.

--- course-projects/4react-products-1front/config/jest.setup.js
+++ course-projects/4react-products-1front/config/jest.setup.js
@@ -1,2 +1,21 @@
 // jest.setup.js
 require('@testing-library/jest-dom');
+
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+if (typeof global.TransformStream === 'undefined') {
+    global.TransformStream = require('web-streams-polyfill/dist/ponyfill').TransformStream;
+}
+if (typeof global.BroadcastChannel === 'undefined') {
+    global.BroadcastChannel = require('broadcast-channel').BroadcastChannel;
+}
+
+global.BroadcastChannel = class {
+    constructor() {}
+    postMessage() {}
+    close() {}
+    onmessage = null;
+};
+
