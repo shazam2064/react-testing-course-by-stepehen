fce1dbf6c66f985eb110cd48f0baf20da4246e84
gabriel <gabriel.salomon.2007@gmail.com>
2025-10-20 10:07:21 -0400

    [5react-products-2back] The get tags tests were added.

---
+++ course-projects/5stack-overflow-2back/tests/tag.test.js
@@ -0,0 +1,100 @@
+const request = require('supertest');
+const app = require('./testUtils');
+const User = require('../models/user.model');
+const Tag = require('../models/tag.model');
+const bcrypt = require('bcryptjs');
+const {mongoConnect, closeConnection} = require("../util/database");
+
+describe('Tag Controller', () => {
+    let validToken;
+
+    beforeAll(async () => {
+        await mongoConnect();
+
+        const passwordHash = await bcrypt.hash('123456', 12);
+        await User.updateOne(
+            {email: 'admin1@test.com'},
+            {
+                $set: {
+                    email: 'admin1@test.com',
+                    password: passwordHash,
+                    name: 'User Test 1',
+                    isAdmin: true,
+                    status: 'I am new!',
+                }
+            },
+            {upsert: true}
+        );
+
+        const loginResponse = await request(app)
+            .post('/auth/login')
+            .send({
+                email: 'admin1@test.com',
+                password: '123456',
+            })
+            .set('Content-Type', 'application/json');
+
+        expect(loginResponse.status).toBe(200);
+        validToken = loginResponse.body.token;
+    });
+
+    afterAll(async () => {
+        await closeConnection();
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('Tag Controller - GET Tags', () => {
+        it('should return 200 and a list of tags', async () => {
+            const mockTags = [
+                {
+                    name: 'New Tag',
+                    questions: [],
+                    description: 'New Tag Description',
+                    _id: '68f63f982eca3e875b58ad7b',
+                    createdAt: '2025-10-20T13:56:40.621Z',
+                    updatedAt: '2025-10-20T13:56:40.621Z',
+                    __v: 0
+                }
+            ];
+
+            jest.spyOn(Tag, 'find').mockResolvedValueOnce(mockTags);
+
+            const response = await request(app)
+                .get('/tags')
+                .set('Authorization', `Bearer ${validToken}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    message: 'Tags fetched successfully',
+                    tags: expect.arrayContaining([
+                        expect.objectContaining({
+                            _id: '68f63f982eca3e875b58ad7b',
+                            name: 'New Tag',
+                            description: 'New Tag Description'
+                        })
+                    ])
+                })
+            );
+        });
+
+        it('should handle errors and return 500', async () => {
+            jest.spyOn(Tag, 'find').mockImplementationOnce(() => Promise.reject(new Error('Database error')));
+
+            const response = await request(app)
+                .get('/tags')
+                .set('Authorization', `Bearer ${validToken}`);
+
+            expect(response.status).toBe(500);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    message: 'Database error'
+                })
+            );
+        });
+    });
+
+});
