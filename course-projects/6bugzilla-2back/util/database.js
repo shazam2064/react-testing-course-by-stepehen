886fd1055477e30f937127f8ffaed880b5363408
gabriel <gabriel.salomon.2007@gmail.com>
2025-11-24 10:31:11 -0500

    [6bugzilla-2back] the database.js was updated to close the connection.

--- course-projects/6bugzilla-2back/util/database.js
+++ course-projects/6bugzilla-2back/util/database.js
@@ -5,10 +5,9 @@ const mongoDb = process.env.MONGO_DB;
 const mongoUser = process.env.MONGO_USER;
 const mongoPass = process.env.MONGO_PASS;
 const mongoCluster = process.env.MONGO_CLUSTER;
-const MONGODB_URI = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoCluster}/${mongoDb}?retryWrites=true&w=majority&ssl=true`
+const MONGODB_URI = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoCluster}/${mongoDb}?retryWrites=true&w=majority&ssl=true`;
 
 const mongoConnect = callback => {
-    console.log('MONGODB_URI: ', MONGODB_URI.replace(mongoPass, '*****'));
     mongoose.connect(MONGODB_URI)
         .then(result => {
             console.log('The MongoDB connection was successful with models: ', result.models);
@@ -17,9 +16,18 @@ const mongoConnect = callback => {
         .catch(err => {
             console.log('There was a MongoDB connection error: ', err);
         });
-}
+};
 
-// -----------------------------------------------------------------------------------------------
+const closeConnection = async () => {
+    try {
+        await mongoose.connection.close();
+        console.log('The MongoDB connection has been closed successfully.');
+    } catch (err) {
+        console.log('Error while closing the MongoDB connection: ', err);
+    }
+};
+
 module.exports = {
     mongoConnect,
+    closeConnection,
 };
