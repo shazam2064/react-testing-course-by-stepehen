2400625358d30b9d296527f78e26ccb4426dbc4e
gabriel <gabriel.salomon.2007@gmail.com>
2025-12-22 09:43:09 -0500

    [7twitter-2back] The testUtils.js was added.

---
+++ course-projects/7twitter-2back/tests/testUtils.js
@@ -0,0 +1,7 @@
+const randomPort = Math.floor(Math.random() * (9999 - 3000 + 1)) + 3000;
+process.env.PORT_NUMBER = randomPort;
+console.log(`Using random port: ${randomPort}`);
+
+const app = require('../app');
+
+module.exports = app;
